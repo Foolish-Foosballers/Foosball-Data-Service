0d979e71adeef6af573c059830d1e36a821ed28b
sarastik <sarastik2@gmail.com>
2017-11-16 15:30:41 -0500

    Testing database

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask
+import os
+import urlparse
 import psycopg2
 
 # Flask app
 app = Flask(__name__)
 
+# PostgreSQL connector
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+cursor = conn.cursor()
 
 query = "SELECT history.player_id, \
             (SELECT h.player_id, COUNT(*) FROM history AS h WHERE h.game_id = history.game_id AND h.player_id IS NOT history.player_id) AS num_games \
@@ -11,7 +26,9 @@ query = "SELECT history.player_id, \
 
 @app.route("/")
 def index():
-    return "Hello Data Service"
+    cursor.execute("SELECT * FROM test")
+    result = cursor.fetchall()
+    return str(result)
 
 if __name__ == "__main__":
     app.run()
