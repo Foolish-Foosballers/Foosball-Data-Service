18a9457590cd895ac3642744f726ce29ebc60c3e
sarastik <sara.stiklickas@ge.com>
2017-12-04 09:55:23 -0500

    Refactoring

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from datetime import date, datetime
-import logging
 from flask_sqlalchemy import SQLAlchemy
+from models import *
 import os
 import json
+import logging
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -12,43 +13,6 @@ db = SQLAlchemy(app)
 gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
 app.logger.setLevel(logging.DEBUG)
-
-class Player(db.Model):
-    Id = db.Column(db.Integer, primary_key=True, nullable=False)
-    FirstName = db.Column(db.String(120), nullable=False)
-    LastName = db.Column(db.String(120), nullable=False)
-    Username = db.Column(db.String(9), unique=True, nullable=False)
-    Email = db.Column(db.String(120), unique=True, nullable=False)
-    SignupDate = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    TotalTimePlayed = db.Column(db.Integer, nullable=False, default=0)
-    GameWins = db.Column(db.Integer, nullable=False, default=0)
-    SeriesWins = db.Column(db.Integer, nullable=False, default=0)
-    TotalPoints = db.Column(db.Integer, nullable=False, default=0)
-    Shutouts = db.Column(db.Integer, nullable=False, default=0)
-    Ranking = db.Column(db.Integer, nullable=False, default=0)
-    def as_dict(self):
-       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-class Game(db.Model):
-    Id = db.Column(db.Integer, primary_key=True, nullable=False)
-    EndTime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
-    Single = db.Column(db.Boolean, nullable=False)
-    LeftScore = db.Column(db.Integer, nullable=False)
-    RightScore = db.Column(db.Integer, nullable=False)
-    WinMargin = db.Column(db.Integer, nullable=False)
-    Winner = db.Column(db.String(10), nullable=False)
-
-class Series(db.Model):
-    Id = db.Column(db.Integer, primary_key=True, nullable=False)
-    NumGames = db.Column(db.Integer, nullable=False)
-    LeftWins = db.Column(db.Integer, nullable=False)
-    RightWins = db.Column(db.Integer, nullable=False)
-
-class History(db.Model):
-    GameId = db.Column(db.Integer, db.ForeignKey('game.Id'), primary_key=True, nullable=False)
-    PlayerId = db.Column(db.Integer, db.ForeignKey('player.Id'), primary_key=True, nullable=False)
-    SeriesId = db.Column(db.Integer, db.ForeignKey('series.Id'), nullable=False)
-    Side = db.Column(db.String(10), nullable=False)
 
 def createTables():
     app.logger.debug('creating tables')
@@ -99,12 +63,20 @@ def homepage():
     <img src="http://loremflickr.com/600/400">
     """.format(time=the_time)
 
-@app.route('/players/<username>')
-def players(username):
-    player = Player.query.filter_by(Username=username).first_or_404()
-    print "here"
-    print type(player)
+@app.route('/players/<id>')
+def playersById(id):
+    player = Player.query.filter_by(Id=id).first_or_404()
     return json.dumps(player.as_dict(), default=jsonSerial)
+
+@app.route('/games/<id>')
+def gamesById(id):
+    game = Game.query.filter_by(Id=id).first_or_404()
+    return json.dumps(game.as_dict(), default=jsonSerial)
+
+@app.route('/series/<id>')
+def seriesById(id):
+    series = Series.query.filter_by(Id=id).first_or_404()
+    return json.dumps(series.as_dict(), default=jsonSerial)
 
 if __name__ == '__main__':
     app.run()
