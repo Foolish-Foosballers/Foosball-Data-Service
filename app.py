22aa12f5552b314ca52bbc6adf7c64296562e515
sarastik <sara.stiklickas@ge.com>
2017-12-04 11:12:36 -0500

    Extracting models

--- app.py
+++ app.py
@@ -8,49 +8,11 @@ import json
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+import models
 
 gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
 app.logger.setLevel(logging.DEBUG)
-
-class Player(db.Model):
-    Id = db.Column(db.Integer, primary_key=True, nullable=False)
-    FirstName = db.Column(db.String(120), nullable=False)
-    LastName = db.Column(db.String(120), nullable=False)
-    Username = db.Column(db.String(9), unique=True, nullable=False)
-    Email = db.Column(db.String(120), unique=True, nullable=False)
-    SignupDate = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    TotalTimePlayed = db.Column(db.Integer, nullable=False, default=0)
-    GameWins = db.Column(db.Integer, nullable=False, default=0)
-    SeriesWins = db.Column(db.Integer, nullable=False, default=0)
-    TotalPoints = db.Column(db.Integer, nullable=False, default=0)
-    Shutouts = db.Column(db.Integer, nullable=False, default=0)
-    Ranking = db.Column(db.Integer, nullable=False, default=0)
-
-    def as_dict(self):
-        """Method for converting model to a dictionary for JSON serializable output"""
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-class Game(db.Model):
-    Id = db.Column(db.Integer, primary_key=True, nullable=False)
-    EndTime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
-    Single = db.Column(db.Boolean, nullable=False)
-    LeftScore = db.Column(db.Integer, nullable=False)
-    RightScore = db.Column(db.Integer, nullable=False)
-    WinMargin = db.Column(db.Integer, nullable=False)
-    Winner = db.Column(db.String(10), nullable=False)
-
-class Series(db.Model):
-    Id = db.Column(db.Integer, primary_key=True, nullable=False)
-    NumGames = db.Column(db.Integer, nullable=False)
-    LeftWins = db.Column(db.Integer, nullable=False)
-    RightWins = db.Column(db.Integer, nullable=False)
-
-class History(db.Model):
-    GameId = db.Column(db.Integer, db.ForeignKey('game.Id'), primary_key=True, nullable=False)
-    PlayerId = db.Column(db.Integer, db.ForeignKey('player.Id'), primary_key=True, nullable=False)
-    SeriesId = db.Column(db.Integer, db.ForeignKey('series.Id'), nullable=False)
-    Side = db.Column(db.String(10), nullable=False)
 
 def createTables():
     app.logger.debug('creating tables')
