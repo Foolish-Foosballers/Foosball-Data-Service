88381d8f06fb3727b833d9c685325db5177462a3
dlernz <daniel.lerner2@gmail.com>
2018-01-03 17:44:57 -0500

    retest matchups

--- app.py
+++ app.py
@@ -124,9 +124,9 @@ def updateRankings():
                 gameTups.append(p1Entry)
                 gameTups.append(p2Entry)
                 matchupsSeen.add((player1.Id, player2.Id))
-                matchupsSeen.add(player2.Id, player1.Id))
+                matchupsSeen.add((player2.Id, player1.Id))
         app.logger.debug(gameTups)
-        
+
         app.logger.debug(matchupsSeen)
     return json.dumps([game.as_dict() for game in games], default=jsonSerial)
 
