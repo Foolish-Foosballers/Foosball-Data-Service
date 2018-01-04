38726f4faafe1d1c5e36e198abaf4424bb8329a0
dlernz <daniel.lerner2@gmail.com>
2018-01-03 19:34:50 -0500

    filter by

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ def updatePlayer(id):
     app.logger.debug(request.json)
     if not request.json:
         abort(400)
-    db.session.query(Players).filter(Id = id).update(request.json)
+    db.session.query(Players).filter_by(Id = id).update(request.json)
     db.session.commit()
     return (json.dumps({"done": "hello"}, default=jsonSerial), 201)
 
